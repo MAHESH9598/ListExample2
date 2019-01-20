512084dfa7269cba2591584e50e7a07344e5bd27
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-21 01:50:43 +0530

    Initial commit

---
+++ ListExample2.py
@@ -0,0 +1,15 @@
+'----------Example 2 -------------'
+lst=[10,'mahi','soft',15]
+print(lst)
+
+nlst=lst*3
+
+print(nlst)
+print(type(nlst))
+
+print(len(lst))
+print(len(nlst))
+
+'List type allows duplications '
+
+'Two convert other type of elements to list we use ----> list()   '
